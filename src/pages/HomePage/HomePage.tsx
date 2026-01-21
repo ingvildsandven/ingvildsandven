138973c6a1108ef2ce5fe322d9810b0eb16083ef
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-01-21 01:39:07 +0100

    [style] scroll effect on desktop implemented for home page (#8)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useRef, useState } from "react";
 import ConstructionCompontent from "../../components/ConstructionComponent/ConstructionComponent";
 import ExperienceComponent from "../../components/ExperienceComponent/ExperienceComponent";
 import SummaryComponent from "../../components/SummaryComponent/SummaryComponent";
 import style from "./HomePage.module.css";
 
 function HomePage() {
+  const summaryRef = useRef(null);
+  const [summaryHeight, setSummaryHeight] = useState(0);
+
+  useEffect(() => {
+    if (!summaryRef.current) return;
+
+    const updateHeight = () => {
+      setSummaryHeight(summaryRef.current.offsetHeight);
+    }
+
+    updateHeight();
+
+    // Dont trigger every pixel change - but when the height is changed
+    const resize = new ResizeObserver(updateHeight);
+    resize.observe(summaryRef.current);
+
+    return () => resize.disconnect();
+  }, [])
+
+  
   return (
-    <main>
-      <h1>Ingvild Kirkaune Sandven</h1>
-      <SummaryComponent />
+    <main className={style.homepage_main} style={{ "--summary-h": `${summaryHeight}px` }}>
+      <div className={style.summary_container} ref={summaryRef}>
+        <h1>Ingvild Kirkaune Sandven</h1>
+        <SummaryComponent />
+      </div>
+
       <div className={style.overlay}>
         <ConstructionCompontent />
         <ExperienceComponent />
